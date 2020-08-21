39e131f79c07131f06164eaad474c8e0bfd01fa7
Mohamed Saqer <mohamed.saqer@outlook.com>
2020-08-22 00:35:48 +0200

    Scroll to section on link click & Set sections as active

--- js/app.js
+++ js/app.js
@@ -24,7 +24,31 @@ var navbar = document.getElementById('navbar__list');
  * Start Helper Functions
  * 
 */
+function activeClass (section) {
+    var isActive = document.querySelector('.your-active-class');
+    if (isActive) {
+        isActive.classList.remove("your-active-class");
+    }
+    document.getElementById(section).classList.add("your-active-class");
+}
 
+function activeButton(button) {
+    var isActive = document.querySelector('.menu__link__active');
+    if (isActive) {
+        isActive.classList.remove("menu__link__active");
+    }
+    document.getElementById(button).classList.add("menu__link__active");
+}
+
+function isInRange(elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (document.documentElement.clientHeight + 300) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 
 /**
@@ -42,6 +66,15 @@ for (let i =1; i <= 3; i++) {
 
 
 // Add class 'active' to section when near top of viewport
+var sectionn = document.querySelectorAll('section').forEach(function (element) {
+    window.addEventListener('scroll', function (event) {
+        if (isInRange(element)) {
+            activeClass(element.id)
+            activeButton(element.id + 'Button')
+        } else {
+        }
+    }, false);
+});
 
 
 // Scroll to anchor ID using scrollTO event
@@ -58,24 +91,14 @@ for (let i =1; i <= 3; i++) {
 
 // Scroll to section on link click
 document.querySelectorAll('.menu__link').forEach(function (element) {
-    element.addEventListener('click', function () {
-    var isActive = document.querySelector('.menu__link__active');
-    if(isActive) {
-        isActive.classList.remove("menu__link__active");
-    }
-    document.getElementById(this.id).classList.add("menu__link__active");
+    element.addEventListener('click', () => {
+        activeButton(element.id);
+        activeClass(element.getAttribute("href").substring(1));
     });
 });
 
 // Set sections as active
-document.querySelectorAll('.menu__link').forEach(function (element) {
-    element.addEventListener('click', function () {
-        var isActive = document.querySelector('.your-active-class');
-        if (isActive) {
-            isActive.classList.remove("your-active-class");
-        }
-        document.getElementById(this.getAttribute("href").substring(1)).classList.add("your-active-class");
-        console.log(this.getAttribute("href").substring(1))
-    });
-});
+// document.querySelectorAll('.menu__link').forEach(function (element) {
+//     element.addEventListener('click', activeClass(element.getAttribute("href").substring(1)));
+// });
 
